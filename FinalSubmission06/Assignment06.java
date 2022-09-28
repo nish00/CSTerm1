e2974f46c6673e9a99a8d3012e76af70f3c621a6
Nishchal Jatwani <nishchal.jatwani@gmail.com>
2022-09-28 11:28:24 -0400

    revising the submission06

--- FinalSubmission06/Assignment06.java
+++ FinalSubmission06/Assignment06.java
@@ -1,27 +1,50 @@
+// Importing the Array class & Scanner class
 import java.util.Arrays;
 import java.util.Scanner;
 
+// Creating main class
 public class Assignment06{
 
+    // Creating main class
     public static void main(String[] args) {
+
+        // Creating Scanner object reference
         Scanner input = new Scanner(System.in);
+        
+        // Creating integer array
         int[] intList = new int[10];
 
+        // Prompting user for number input
         System.out.println("Please enter the required 10 numbers: ");
+        
+        // Using for loop for integer datatype input
         for(int i = 0; i<intList.length; i++) {
             intList[i] = input.nextInt();
         }
+        // Printing the final array 
         System.out.println(revArray(intList));
+
+        // Closing the Scanner class
         input.close();
     }
 
+
+    // Creating the method for reversing the array
     public static String revArray(int[] arrayIn) {
+
+        // Copying the array into the new array
         int[] newArray = new int[arrayIn.length];
+
+        // Initiating the integer as counter
         int n = 0 ;
+
+        // Using for loop for reversing the elements and storing in new array
         for (int i=arrayIn.length; i>0; i--) {
             newArray[n] = arrayIn[i-1];
             n++;
         }
+
+        // returning the final reversed array
         return Arrays.toString(newArray);
     }
 }
--- FinalSubmission06/Assignment0602.java
+++ FinalSubmission06/Assignment0602.java
@@ -1,3 +1,59 @@
-class Assignment0602{
+// Importing Scanner class 
+import java.util.Scanner;
+
+// Declaration of class
+public class Assignment0602 {
+
+    // Declarartion of main class
+    public static void main(String[] args) {
     
+    // Creating scanner object reference
+    Scanner input = new Scanner(System.in);
+
+    // Creating the double array
+    double[] decArray = new double[10];
+
+    // Prompting user for the input
+    System.out.println("Enter double values for average: ");
+        
+        // Using for-loop for double datatype input
+        for(int i = 0; i<decArray.length;i++) {
+            decArray[i] = input.nextDouble();
+        }
+
+        // Invoking the average method 
+        double finalAverage = average(decArray);
+
+        // Printing the final output on the terminal
+        System.out.println("The final average of the numbers " + finalAverage);
+    }
+
+    // Creating average method for int datatype input
+    public static int average(int[] array) {
+        // Initiating the total
+        int sum = 0;
+        // Using for loop for adding all the elements in the array
+        for (int i = 0; i<array.length; i++) {
+            sum += array[i];
+        }
+        // Averaging the sum 
+        int avArray = (sum/array.length);
+        
+        // Returning the final array
+        return avArray;
+    }
+    // Creating average method for double datatype input
+    public static double average(double[] array) {
+        // Initiating the total
+        double sum = 0;
+        // Using for loop for adding all the elements in the array
+        for(int i = 0; i<array.length; i++) {
+            sum += array[i];
+        }
+        // Averaging the sum
+        double avArray = (sum/array.length);
+    
+        // Returning the final array
+        return avArray;
+    }
 }
